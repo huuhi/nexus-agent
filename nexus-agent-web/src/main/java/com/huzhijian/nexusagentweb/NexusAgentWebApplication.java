80aa4357ef6ec6d6b42faa6be263faea18d3eee8
hu <3108967414@qq.com>
2026-04-14 10:17:43 +0800

    Initial commit

---
+++ nexus-agent-web/src/main/java/com/huzhijian/nexusagentweb/NexusAgentWebApplication.java
@@ -0,0 +1,13 @@
+package com.huzhijian.nexusagentweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAgentWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAgentWebApplication.class, args);
+    }
+
+}
---
+++ nexus-agent-web/src/test/java/com/huzhijian/nexusagentweb/NexusAgentWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huzhijian.nexusagentweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusAgentWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
